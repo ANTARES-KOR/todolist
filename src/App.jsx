1282b8fd662b70bcee497164aedc234696f20b65
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2022-09-28 19:38:50 +0900

    [Feat] 사용성 개선

--- src/App.jsx
+++ src/App.jsx
@@ -5,88 +5,114 @@ import classNames from "classnames";
 
 import { BsTrash } from "react-icons/bs";
 import { useEffect } from "react";
+import { useCallback } from "react";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+  const [inputError, setInputError] = useState();
 
-  const handleTodoSubmit = (e) => {
-    e.preventDefault();
-    setTodos((prev) => {
-      const newTodo = {
-        id: nanoid(),
-        content: inputValue,
-        isDone: false,
-      };
+  const handleTodoSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-      return [...prev, newTodo];
-    });
-    setInputValue("");
-  };
+      if (inputValue.trim() === "") {
+        setInputError("공백이 아닌 내용을 입력해주세요.");
+        return;
+      }
 
-  const removeTodoById = (id) => {
+      setTodos((prev) => {
+        const newTodo = {
+          id: nanoid(),
+          content: inputValue.trim(),
+          isDone: false,
+        };
+        return [newTodo, ...prev];
+      });
+      setInputValue("");
+    },
+    [inputValue]
+  );
+
+  const removeTodoById = useCallback((id) => {
     setTodos((prev) => {
       return prev.filter((todo) => todo.id !== id);
     });
-  };
+  }, []);
 
-  const changeTodoDoneStatus = (id) => {
+  const changeTodoDoneStatus = useCallback((id) => {
     setTodos((prev) => {
       const targetIndex = prev.findIndex((todo) => todo.id === id);
       const newTodos = [...prev];
       const [targetTodo] = newTodos.splice(targetIndex, 1);
-      return [...newTodos, { ...targetTodo, isDone: !targetTodo.isDone }];
+
+      if (targetTodo.isDone) {
+        // 완료된 상태에서 미완료로 변경할 경우 맨 앞으로 이동
+        return [{ ...targetTodo, isDone: !targetTodo.isDone }, ...newTodos];
+      } else {
+        // 미완료 상태에서 완료로 변경할 경우 맨 뒤로 이동
+        return [...newTodos, { ...targetTodo, isDone: !targetTodo.isDone }];
+      }
     });
-  };
+  }, []);
 
   return (
     <main className={styles.main_background}>
       <section className={styles.content_section}>
         <form className={styles.todo_form} onSubmit={handleTodoSubmit}>
-          <input
-            placeholder="할 일을 입력하세요..."
-            className={styles.todo_input}
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-          />
-          <button type="submit" className={styles.todo_submit_button}>
-            제출
-          </button>
+          <div className={styles.todo_form__input_container}>
+            <input
+              placeholder="할 일을 입력하세요..."
+              className={styles.todo_input}
+              onChange={(e) => {
+                setInputError();
+                setInputValue(e.target.value);
+              }}
+              value={inputValue}
+            />
+            <button type="submit" className={styles.todo_submit_button}>
+              제출
+            </button>
+          </div>
+          {inputError && (
+            <p className={styles.todo_form__error_message}>{inputError}</p>
+          )}
         </form>
         <article className={styles.todo_container}>
-          {todos.map(({ id, content, isDone }, index) => {
-            return (
-              <label
-                key={id}
-                id={`todo-checkbox-${index}`}
-                className={styles.todo}
-              >
-                <div className={styles.todo__label}>
-                  <input
-                    className={styles.todo__label__checkbox}
-                    type="checkbox"
+          <ul className={styles.todo_container__inner}>
+            {todos.map(({ id, content, isDone }, index) => {
+              return (
+                <li key={id} className={styles.todo}>
+                  <label
                     id={`todo-checkbox-${index}`}
-                    onChange={() => {
-                      changeTodoDoneStatus(id);
+                    className={styles.todo__label}
+                  >
+                    <input
+                      className={styles.todo__label__checkbox}
+                      type="checkbox"
+                      id={`todo-checkbox-${index}`}
+                      onChange={() => {
+                        changeTodoDoneStatus(id);
+                      }}
+                      value={isDone}
+                    />
+                    <span className={isDone ? styles.todo__done : undefined}>
+                      {content}
+                    </span>
+                  </label>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      confirm("정말 삭제하시겠습니까?") && removeTodoById(id);
                     }}
-                    value={isDone}
-                  />
-                  <span className={isDone ? styles.todo__done : undefined}>
-                    {content}
-                  </span>
-                </div>
-                <button
-                  type="button"
-                  onClick={() => {
-                    removeTodoById(id);
-                  }}
-                  className={styles.todo__remove_button}
-                >
-                  <BsTrash />
-                </button>
-              </label>
-            );
-          })}
+                    className={styles.todo__remove_button}
+                  >
+                    <BsTrash />
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
         </article>
       </section>
     </main>
