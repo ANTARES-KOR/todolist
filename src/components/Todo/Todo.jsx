fb3d16a83f34a0c37146a1a804aacb58796b3d01
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2022-10-05 21:44:05 +0900

    [Feat] 수정 기능 구현

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,14 @@ import { useCallback } from "react";
 import { BsTrash } from "react-icons/bs";
 import classNames from "classnames";
 import styles from "./Todo.module.css";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+const TodoMode = {
+  EDIT: "EDIT",
+  VIEW: "VIEW",
+};
 
 export const Todo = ({ data, index, setTodos }) => {
   const { id, content, isDone } = data;
@@ -12,49 +20,124 @@ export const Todo = ({ data, index, setTodos }) => {
     });
   }, []);
 
-  const changeTodoDoneStatus = useCallback((id) => {
-    setTodos((prev) => {
-      const targetIndex = prev.findIndex((todo) => todo.id === id);
-      const newTodos = [...prev];
-      const todosBeforeTarget = newTodos.slice(0, targetIndex);
-      const todosAfterTarget = newTodos.slice(targetIndex + 1);
-      const [targetTodo] = newTodos.splice(targetIndex, 1);
+  const changeTodoDoneStatus = useCallback(
+    (id) => {
+      setTodos((prev) => {
+        const targetIndex = prev.findIndex((todo) => todo.id === id);
+        const newTodos = [...prev];
+        const todosBeforeTarget = newTodos.slice(0, targetIndex);
+        const todosAfterTarget = newTodos.slice(targetIndex + 1);
+        const [targetTodo] = newTodos.splice(targetIndex, 1);
 
-      return [
-        ...todosBeforeTarget,
-        { ...targetTodo, isDone: !targetTodo.isDone },
-        ...todosAfterTarget,
-      ];
-    });
-  }, []);
+        return [
+          ...todosBeforeTarget,
+          { ...targetTodo, isDone: !targetTodo.isDone },
+          ...todosAfterTarget,
+        ];
+      });
+    },
+    [setTodos]
+  );
+
+  const changeTodoText = useCallback(
+    (id, text) => {
+      setTodos((prev) => {
+        const targetIndex = prev.findIndex((todo) => todo.id === id);
+        const newTodos = [...prev];
+        const todosBeforeTarget = newTodos.slice(0, targetIndex);
+        const todosAfterTarget = newTodos.slice(targetIndex + 1);
+        const [targetTodo] = newTodos.splice(targetIndex, 1);
+
+        return [
+          ...todosBeforeTarget,
+          { ...targetTodo, content: text },
+          ...todosAfterTarget,
+        ];
+      });
+    },
+    [setTodos]
+  );
+
+  const [todoMode, setTodoMode] = useState(TodoMode.VIEW);
+
+  const todoEditInputRef = useRef();
+
+  useEffect(() => {
+    if (todoMode === TodoMode.EDIT && todoEditInputRef.current) {
+      todoEditInputRef.current.focus();
+      todoEditInputRef.current.value = content;
+    }
+  }, [todoMode, content]);
 
   return (
     <li className={styles.todo}>
-      <div id={`todo-checkbox-${index}`} className={styles.todo__label}>
-        <input
-          className={styles.todo__label__checkbox}
-          type="checkbox"
-          id={`todo-checkbox-${index}`}
-          onChange={() => changeTodoDoneStatus(id)}
-          checked={isDone}
-        />
-        <span
-          className={classNames(styles.prevent_select, {
-            [styles.todo__done]: isDone,
-          })}
+      {todoMode === TodoMode.VIEW && (
+        <>
+          <div
+            id={`todo-checkbox-${index}`}
+            className={styles.todo__label}
+            onDoubleClick={() => setTodoMode(TodoMode.EDIT)}
+          >
+            <input
+              className={styles.todo__label__checkbox}
+              type="checkbox"
+              id={`todo-checkbox-${index}`}
+              onChange={() => changeTodoDoneStatus(id)}
+              checked={isDone}
+            />
+            <span
+              className={classNames(styles.prevent_select, {
+                [styles.todo__done]: isDone,
+              })}
+            >
+              {content}
+            </span>
+          </div>
+          <button
+            type="button"
+            onClick={() => {
+              confirm("정말 삭제하시겠습니까?") && removeTodoById(id);
+            }}
+            className={styles.todo__remove_button}
+          >
+            <BsTrash />
+          </button>
+        </>
+      )}
+      {todoMode === TodoMode.EDIT && (
+        <form
+          className={styles["todo__edit-form"]}
+          onSubmit={(e) => {
+            e.preventDefault();
+            changeTodoText(id, todoEditInputRef.current?.value);
+            setTodoMode(TodoMode.VIEW);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Escape") {
+              setTodoMode(TodoMode.VIEW);
+              e.stopPropagation();
+            }
+          }}
         >
-          {content}
-        </span>
-      </div>
-      <button
-        type="button"
-        onClick={() => {
-          confirm("정말 삭제하시겠습니까?") && removeTodoById(id);
-        }}
-        className={styles.todo__remove_button}
-      >
-        <BsTrash />
-      </button>
+          <input
+            ref={todoEditInputRef}
+            type="text"
+            className={styles["todo__edit-form__input"]}
+          />
+          <button
+            onClick={() => {
+              setTodoMode(TodoMode.VIEW);
+            }}
+            type="button"
+            className={styles["todo__edit-form__cancel"]}
+          >
+            취소
+          </button>
+          <button type="submit" className={styles["todo__edit-form__submit"]}>
+            저장
+          </button>
+        </form>
+      )}
     </li>
   );
 };
